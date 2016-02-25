86c4398f32fc273eef316314eb702cd4b5bc62c1
EGOIST <0x142857@gmail.com>
2016-02-25 11:34:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# decheck [![NPM version](https://img.shields.io/npm/v/decheck.svg)](https://npmjs.com/package/decheck) [![NPM downloads](https://img.shields.io/npm/dm/decheck.svg)](https://npmjs.com/package/decheck)
+
+> Checkout the dependencies an NPM module depends on.
+
+## Purpose
+
+I always want to browse a modules's `package.json` to find new modules that are used by it. With [octo-link](https://github.com/octo-linker/chrome-extension) I can click to know what the module is all about, but click every time? Nope, I want all in one time.
+
+![preview](https://ooo.0o0.ooo/2016/02/24/56ce762bd0b80.png)
+
+## Install
+
+```bash
+$ npm install -g decheck
+```
+
+## Usage
+
+```bash
+$ decheck -h
+
+  Usage:
+
+    decheck <moduleName> [version]
+
+    -v/--version:    Print version
+    -h/--help:       Print help
+    -r/--registry:   Set custom npm registry
+```
+
+## License
+
+MIT © [EGOIST](https://github.com/egoist)
---
+++ cli.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+'use strict'
+
+const co = require('co')
+const fetch = require('node-fetch')
+const minimist = require('minimist')
+const toArray = require('object2array')
+const isTaken = require('is-taken')
+const Spin = require('io-spin')
+const update = require('update-notifier')
+const pkg = require('./package')
+require('colorful').toxic()
+
+const argv = minimist(process.argv.slice(2), {
+	alias: {
+		h: 'help',
+		v: 'version',
+		r: 'registry'
+	}
+})
+
+const spin = new Spin('Box1', 'Checking')
+
+// update notify
+update({pkg}).notify()
+
+if (argv.help) {
+	console.log(`
+  Usage:
+
+    decheck <moduleName> [version]
+
+    -v/--version:    Print version
+    -h/--help:       Print help
+    -r/--registry:   Set custom npm registry
+	`)
+	process.exit()
+}
+
+if (argv.version) {
+	console.log(pkg.version)
+	process.exit()
+}
+
+const moduleName = argv._[0]
+const moduleVersion = argv._[1]
+
+if (!moduleName) {
+	console.log('Required a module name')
+	process.exit(1)
+}
+
+let registry = argv.registry || 'https://registry.npmjs.org/'
+if (argv.cn) {
+	registry = 'https://registry.npm.taobao.org/'
+}
+
+co(function* () {
+	spin.start()
+	const pkg = yield fetch(`${registry}${moduleName}`).then(data => data.json())
+	const version = moduleVersion || pkg['dist-tags']['latest']
+	let deps = pkg.versions[version].dependencies
+
+	if (!deps) {
+		spin.stop()
+		console.log('Sorry, but this package depends on nothing.')
+		process.exit()
+	}
+	deps = toArray(deps)
+
+	const depsData = yield deps.map(dep => {
+		return isTaken(dep.key, {registry, timeout: 10000}).then(data => data.versions[data['dist-tags'].latest])
+	})
+	spin.stop()
+	console.log(depsData.map(dep => {
+		return `
+  ${dep.name.bold} ${'v'.gray}${dep.version.gray}
+  ${dep.description.cyan}
+  ${`https://npmjs.org/package/${dep.name}`.gray}
+	`
+	}).join(''))
+}).catch(e => console.log(e.stack))
